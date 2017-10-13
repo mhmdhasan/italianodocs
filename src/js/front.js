6fa0cea0d41784dacc6e8c9ac8bc62ebd54f73ba
Ondřej Švestka <svestka.ondrej@gmail.com>
2017-10-13 10:32:42 +0200

    changed front.js - demo(), utils() gone, added new snippet for demo style switcher.

--- src/js/front.js
+++ src/js/front.js
@@ -1,16 +1,15 @@
 $(function () {
 
-    utils();
-    demo();
+    // ------------------------------------------------------ //
+    // For demo purposes, can be deleted
+    // ------------------------------------------------------ //
 
-});
-
-/* for demo purpose only - can be deleted */
-
-function demo() {
+    var stylesheet = $('link#theme-stylesheet');
+    $( "<link id='new-stylesheet' rel='stylesheet'>" ).insertAfter(stylesheet);
+    var alternateColour = $('link#new-stylesheet');
 
     if ($.cookie("theme_csspath")) {
-        $('link#theme-stylesheet').attr("href", $.cookie("theme_csspath"));
+        alternateColour.attr("href", $.cookie("theme_csspath"));
     }
 
     $("#colour").change(function () {
@@ -19,72 +18,13 @@ function demo() {
 
             var theme_csspath = 'css/style.' + $(this).val() + '.css';
 
-            $('link#theme-stylesheet').attr("href", theme_csspath);
+            alternateColour.attr("href", theme_csspath);
 
             $.cookie("theme_csspath", theme_csspath, { expires: 365, path: document.URL.substr(0, document.URL.lastIndexOf('/')) });
+
         }
 
         return false;
     });
 
-    $("#layout").change(function () {
-
-        if ($(this).val() !== '') {
-
-            var theme_layout = $(this).val();
-
-            $('body').removeClass('wide');
-            $('body').removeClass('boxed');
-
-            $('body').addClass(theme_layout);
-
-            $.cookie("theme_layout", theme_layout, { expires: 365, path: document.URL.substr(0, document.URL.lastIndexOf('/')) });
-        }
-
-        return false;
-    });
-
-
-}
-
-function utils() {
-
-    /* click on the box activates the link in it */
-
-    $('.box.clickable').on('click', function (e) {
-
-        window.location = $(this).find('a').attr('href');
-    });
-    /* external links in new window*/
-
-    $('.external').on('click', function (e) {
-
-        e.preventDefault();
-        window.open($(this).attr("href"));
-    });
-    /* animated scrolling */
-
-    $('.scroll-to, .scroll-to-top').click(function (event) {
-
-        var full_url = this.href;
-        var parts = full_url.split("#");
-        if (parts.length > 1) {
-
-            scrollTo(full_url);
-            event.preventDefault();
-        }
-    });
-    function scrollTo(full_url) {
-        var parts = full_url.split("#");
-        var trgt = parts[1];
-        var target_offset = $("#" + trgt).offset();
-        var target_top = target_offset.top - 100;
-        if (target_top < 0) {
-            target_top = 0;
-        }
-
-        $('html, body').animate({
-            scrollTop: target_top
-        }, 1000);
-    }
-}
+});
